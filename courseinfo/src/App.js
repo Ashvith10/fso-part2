b72253af8556ea8421fff84569234fa43d880709
Ashvith Shetty <ashvithshetty10@gmail.com>
2023-03-17 23:08:41 +0530

    chore: Refactor into multiple components

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,32 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-const Total = ({ sum }) => <b>total of {sum} exercises</b>
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) =>
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />
-    <Part
-      part={parts[3]}
-    />
-  </>
-
-const Course = ({ course }) =>
-    <div>
-        <Header course={course.name} />
-        <Content parts={course.parts} />
-        <Total sum={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
-    </div>
+import Course from './components/Course'
 
 const App = () => {
   const course = {
