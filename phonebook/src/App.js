8f203410410573e1205cb5c8c915d812d1480476
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-03-26 19:37:09 +0530

    chore: Add support for form input

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,7 +1,33 @@
+import {useState} from 'react'
+
 const App = () => {
-  return (
-    <div>Hello world</div>
-  );
+    const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState('')
+
+    const handleNameInput = (event) => setNewName(event.target.value)
+
+    const addPerson = (event) => {
+        event.preventDefault()
+        setPersons(prevState => [...prevState, {name: newName}])
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form>
+                <div>
+                    name: <input type="textbox" value={newName} onChange={handleNameInput} />
+                </div>
+                <div>
+                    <button type="submit" onClick={addPerson}>add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <div>
+                {persons.map((person, id) => <div key={id}>{person.name}</div>)}
+            </div>
+        </div>
+    );
 }
 
 export default App;
