8aa70386ea338008e6982fdff7903d73212ea0f6
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-03-31 06:15:50 +0530

    chore: Add phone number to phone book

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,15 +3,17 @@ import {useState} from 'react'
 const App = () => {
     const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
     const handleNameInput = (event) => setNewName(event.target.value)
-
+    const handleNumberInput = (event) => setNewNumber(event.target.value)
+    
     const addPerson = (event) => {
         event.preventDefault()
-        if (!persons.some(person => person.name === newName)) {
-            setPersons(prevState => [...prevState, {name: newName}])
-        } else {
+        if (persons.some(person => person.name === newName)) {
             alert(`${newName} is already added to phonebook`)
+        } else {
+            setPersons(prevState => [...prevState, {name: newName, number: newNumber}])
         }
     }
 
@@ -23,15 +25,19 @@ const App = () => {
                     name: <input type="textbox" value={newName} onChange={handleNameInput} />
                 </div>
                 <div>
+                    number: <input type="textbox" value={newNumber} onChange={handleNumberInput} />
+                </div>
+
+                <div>
                     <button type="submit" onClick={addPerson}>add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
             <div>
-                {persons.map((person, id) => <div key={id}>{person.name}</div>)}
+                {persons.map((person, id) => <div key={id}>{person.name} {person.number}</div>)}
             </div>
         </div>
-    );
+    )
 }
 
 export default App;
