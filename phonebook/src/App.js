ddc552b1676a90a639cd5206ba540319a812fb10
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-04-05 12:39:20 +0530

    chore: Use axios to read from json-server

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,4 +1,5 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -21,6 +22,12 @@ const App = () => {
             setPersons(prevState => [...prevState, {name: newName, number: newNumber}])
         }
     }
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => setPersons(response.data))
+    }, [])
 
     return (
         <div>
