b25e14e314a5bc85cad5d23e95a0127694a88e55
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-04-16 22:03:09 +0530

    chore: Show error message

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,6 +3,7 @@ import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import Success from './components/Success'
+import Error from './components/Error'
 import personService from './services/person'
 import './App.css'
 
@@ -12,6 +13,7 @@ const App = () => {
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [successMessage, setSuccessMessage] = useState(null)
+    const [errorMessage, setErrorMessage] = useState(null)
 
     const handleSearchInput = (event) => setSearchToken(event.target.value)
     const handleNameInput = (event) => setNewName(event.target.value)
@@ -24,9 +26,16 @@ const App = () => {
             if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
                 personService
                     .update(person.id, {name: newName, number: newNumber})
-                    .then(response => setPersons(prevState => prevState.map(obj => obj.id === response.data.id ? response.data : obj)))
-                setSuccessMessage(`Updated ${newName}`)
-                setTimeout(() => setSuccessMessage(null), 5000)
+                    .then(response => {
+                        setPersons(prevState => prevState.map(obj => obj.id === response.data.id ? response.data : obj))
+                        setSuccessMessage(`Updated ${newName}`)
+                        setTimeout(() => setSuccessMessage(null), 5000)
+                    })
+                    .catch(error => {
+                        setPersons(prevState => prevState.filter(obj => person.id  !== obj.id))
+                        setErrorMessage(`Information of ${newName} has already been removed from server`)
+                        setTimeout(() => setErrorMessage(null), 5000)
+                    })
             }
         } else {
             personService
@@ -48,6 +57,10 @@ const App = () => {
                         person.id !== persons[id].id
                     )
                 ))
+                .catch(error => {
+                    setErrorMessage(`Information of ${persons[id].name} has already been removed from server`)
+                    setTimeout(() => setErrorMessage(null), 5000)
+                })
         }
     }
 
@@ -61,6 +74,7 @@ const App = () => {
         <div>
             <h2>Phonebook</h2>
             <Success message={successMessage}/>
+            <Error message={errorMessage}/>
             <Filter searchToken={searchToken} handleSearchInput={handleSearchInput} />
             <h2>Add a new</h2>
             <PersonForm newName={newName} handleNameInput={handleNameInput} newNumber={newNumber} handleNumberInput={handleNumberInput} addPerson={addPerson} />
