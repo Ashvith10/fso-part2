ac4879f4f4e0905ab72c9148a9e25fb6938a74e5
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-04-16 18:05:16 +0530

    chore: Show success message

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,13 +2,16 @@ import {useState, useEffect} from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Success from './components/Success'
 import personService from './services/person'
+import './App.css'
 
 const App = () => {
     const [persons, setPersons] = useState([])
     const [searchToken, setSearchToken] = useState('')
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [successMessage, setSuccessMessage] = useState(null)
 
     const handleSearchInput = (event) => setSearchToken(event.target.value)
     const handleNameInput = (event) => setNewName(event.target.value)
@@ -21,16 +24,16 @@ const App = () => {
             if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
                 personService
                     .update(person.id, {name: newName, number: newNumber})
-                    .then(response =>
-                        setPersons(prevState => 
-                                prevState.map(obj => obj.id === response.data.id ? response.data : obj)
-                        )
-                    )
+                    .then(response => setPersons(prevState => prevState.map(obj => obj.id === response.data.id ? response.data : obj)))
+                setSuccessMessage(`Updated ${newName}`)
+                setTimeout(() => setSuccessMessage(null), 5000)
             }
         } else {
             personService
                 .create({name: newName, number: newNumber})
                 .then(response => setPersons(prevState => [...prevState, {...response.data}]))
+            setSuccessMessage(`Added ${newName}`)
+            setTimeout(() => setSuccessMessage(null), 5000)
         }
     }
 
@@ -57,6 +60,7 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <Success message={successMessage}/>
             <Filter searchToken={searchToken} handleSearchInput={handleSearchInput} />
             <h2>Add a new</h2>
             <PersonForm newName={newName} handleNameInput={handleNameInput} newNumber={newNumber} handleNumberInput={handleNumberInput} addPerson={addPerson} />
